52149ffec83d837ba2d36d1388fc1ff366ebf8db
VolfsonAlexandr <sancho11alex@gmail.com>
2018-01-23 18:15:58 +0200

    changes for test3

---
+++ Combats_Test/GamerArgs2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combats_Test
+{
+    public class GamerArgs: EventArgs
+    {
+        public string name;
+        public int hp;
+
+
+        public GamerArgs(string name, int hp)
+        {
+            this.name = name;
+            this.hp = hp;
+        }
+    }
+}
